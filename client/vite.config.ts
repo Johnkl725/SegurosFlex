98a80d231cd1aaae644b5b7442177a1a19060cff
Johnkl725 <john.castillo1@unmsm.edu.pe>
2025-01-26 13:26:24 -0500

    corrigiendo frontend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()], // Usar el plugin de Tailwind CSS para Vite
-});
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
